06f8e01db7ac87085ded0f2115d08931b54f854e
Renee Paczkowski <rpaczkowski@gmail.com>
2022-04-25 19:01:53 -0400

    created variable population

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstName = "Ren";
+let lastName = "Paczkowski";
+const fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+let population = "674,841";
+
+console.log(population);
+
